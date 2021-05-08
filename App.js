b9474a0628687ef68a62e7cb702bbf81941182a1
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-05-08 16:39:16 +0700

    Display info about russia's covid cases

--- App.js
+++ App.js
@@ -3,18 +3,49 @@ import { StyleSheet, View, Button, Text } from 'react-native';
 import { Population } from './src/components/Population';
 
 export default function App() {
-  const [population, setPopulation] = useState();
+  const [population, setPopulation] = useState({
+    cases: 0,
+    recovered: 0,
+    death: 0,
+    todayCases: 0,
+    todayDeaths: 0,
+    todayRecovered: 0,
+  });
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+
   useEffect(() => {
-    setPopulation(0);
+    pressHandler();
   }, []);
+
+  const pressHandler = () => {
+    fetch(
+      'https://corona.lmao.ninja/v2/countries/Russia?yesterday&strict&query',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setPopulation(result))
+      .catch((error) => console.log('error', error));
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.block}>
-        <Text style={styles.text}>{'Количество зараженных в России:'}</Text>
+        <Text style={styles.title}>{'Количество зараженных в России'}</Text>
       </View>
       <View style={styles.container}>
         <Population population={population} />
+        <View style={styles.update}>
+          <Button
+            style={styles.updatebtn}
+            color="#ac42d4"
+            title="Обновить"
+            onPress={pressHandler}
+          />
+        </View>
       </View>
     </View>
   );
@@ -23,6 +54,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
+    height: '100%',
   },
   text: {
     display: 'flex',
@@ -31,10 +63,27 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 20,
   },
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontSize: 20,
+  },
   block: {
-    backgroundColor: '#fff',
+    backgroundColor: '#912ab7',
     alignItems: 'center',
     marginBottom: 15,
-    marginTop: 50,
+    marginTop: 45,
+    padding: 10,
+  },
+  update: {
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  updatebtn: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '100%',
   },
 });
--- src/components/Population.js
+++ src/components/Population.js
@@ -4,7 +4,26 @@ import { View, StyleSheet, Text } from 'react-native';
 export const Population = ({ population }) => {
   return (
     <View style={styles.block}>
-      <Text style={styles.text}>{population}</Text>
+      <Text style={styles.text}>Случаев заражения: {population.cases}</Text>
+      <Text style={styles.text}>Смертей: {population.deaths}</Text>
+      <Text style={styles.text}>Выздоровевших: {population.recovered}</Text>
+      <View
+        style={{
+          margin: 5,
+          borderBottomColor: '#dab1e0',
+          borderBottomWidth: 1,
+          alignSelf: 'stretch',
+        }}
+      />
+      <Text style={styles.text}>
+        Заражений за день: {population.todayCases}
+      </Text>
+      <Text style={styles.text}>Смертей за день: {population.todayDeaths}</Text>
+      <Text style={styles.text}>
+        Выздоровевших за день: {population.todayRecovered}
+      </Text>
+      {/* {/* <Text style={styles.text}>{population} человек</Text>
+      <Text style={styles.text}>{population} человек</Text> */}
     </View>
   );
 };
@@ -14,12 +33,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 15,
     marginTop: 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: '#d391de',
+    borderColor: '#d391de',
+    padding: 10,
+    margin: 10,
   },
-  text: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'black',
-    fontSize: 20,
-  },
+  text: { textAlign: 'left', color: 'white', fontSize: 20 },
 });
